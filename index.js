77c89938c30357548c264cd380a90deb049b8c03
Newton Migosi <newtonmigosi@gmail.com>
2018-04-17 13:03:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,5 +52,8 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber='') {
-  if (cardNumber='') {return "Sorry, we don't have a credit card on file for you. "}
+  if (cardNumber==='') {
+    return "Sorry, we don't have a credit card on file for you."
+  }
+  var cost = tota
 }
