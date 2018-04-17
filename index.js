cbe87fc12841f447e343b6b1b6a5c3c37cb7efa6
Newton Migosi <newtonmigosi@gmail.com>
2018-04-17 13:06:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,5 +57,5 @@ function placeOrder(cardNumber='') {
   }
   var cost = total();
   cart = [];
-  return `Your total cost is ${cost}, which will be charged to the card ${cardNumber}.`;
+  return `Your total cost is $${cost}, which will be charged to the card ${cardNumber}.`;
 }
