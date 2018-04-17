f7ee93b6698b0fe4b162813d4b28b7628506cfee
Newton Migosi <newtonmigosi@gmail.com>
2018-04-17 12:33:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- temp = { itemName: item, itemPrice: }
+  var price = Math.floor(Math.random() * 100);
+  var temp = { itemName: item, itemPrice: price};
+  cart.push(temp);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
