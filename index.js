258c41c0e921025684b019946c8f7ca91ef4571d
Newton Migosi <newtonmigosi@gmail.com>
2018-04-17 13:06:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,10 +53,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber='') {
   if (cardNumber==='') {
-    return "Sorry, we don't have a credit card on file for you."
+    return "Sorry, we don't have a credit card on file for you.";
   }
   var cost = total();
   cart = [];
   return `Your total cost is ${cost}, which will be charged to the card ${cardNumber}.`;
-  
 }
